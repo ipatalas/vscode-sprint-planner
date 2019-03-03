6504ef61d91b25d2d9a6824e6277d21504dea501
Ireneusz Patalas <fido@list.pl>
2019-03-03 20:18:55 +0100

    Add basic language configuration

--- src/extension.ts
+++ src/extension.ts
@@ -2,21 +2,15 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const documentSelector = [
+	{ language: 'planner', scheme: 'file' },
+	{ language: 'planner', scheme: 'untitled' },
+];
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "vscode-sprint-planner" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
+	let disposable = vscode.commands.registerCommand('sprintplanner.publish', () => {
 		vscode.window.showInformationMessage('Hello World!');
 	});
 
