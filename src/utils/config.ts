8e3622f655ba4c0b6b2ca1a55d9503e70eaffb9b
Ireneusz Patalas <fido@list.pl>
2019-12-01 18:35:03 +0100

    Don't reload snippets when they have not been changed

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,117 +4,122 @@ import { Logger } from './logger';
 import Axios from 'axios';
 
 const ConfigurationKey = 'planner.azure-devops';
+const SnippetsConfigurationKey = 'planner.azure-devops.snippets';
 
 export class Configuration implements vsc.Disposable {
-	public organization: string | undefined;
-	public project: string | undefined;
-	public team: string | undefined;
-	public token: string | undefined;
-	public debug!: boolean;
-	public defaultActivity: string | undefined;
-	public snippets: { [name: string]: string } | undefined;
+    public organization: string | undefined;
+    public project: string | undefined;
+    public team: string | undefined;
+    public token: string | undefined;
+    public debug!: boolean;
+    public defaultActivity: string | undefined;
+    public snippets: { [name: string]: string } | undefined;
 
-	private _onDidChange: vsc.EventEmitter<Configuration>;
-	private _eventHandler: vsc.Disposable;
+    private _onDidChange: vsc.EventEmitter<Configuration>;
+    private _eventHandler: vsc.Disposable;
 
-	get onDidChange() {
-		return this._onDidChange.event;
-	}
+    get onDidChange() {
+        return this._onDidChange.event;
+    }
 
-	get isValid() {
-		return !!this.organization && !!this.project && !!this.team && !!this.token;
-	}
+    get isValid() {
+        return !!this.organization && !!this.project && !!this.team && !!this.token;
+    }
 
-	constructor(private logger: Logger) {
-		this._onDidChange = new vsc.EventEmitter<Configuration>();
+    constructor(private logger: Logger) {
+        this._onDidChange = new vsc.EventEmitter<Configuration>();
 
-		this.load().then(() => {
-			logger.log('Configuration loaded');
-		});
+        this.load(true).then(() => {
+            logger.log('Configuration loaded');
+        });
 
-		this._eventHandler = vsc.workspace.onDidChangeConfiguration(event => {
-			if (event.affectsConfiguration(ConfigurationKey)) {
-				this.load().then(() => {
-					logger.log('Configuration reloaded');
-					this._onDidChange.fire(this);
-				});
-			}
-		});
-	}
+        this._eventHandler = vsc.workspace.onDidChangeConfiguration(event => {
+            if (event.affectsConfiguration(ConfigurationKey)) {
+                const snippetsChanged = event.affectsConfiguration(SnippetsConfigurationKey);
 
-	private async load() {
-		const config = vsc.workspace.getConfiguration(ConfigurationKey);
-		this.organization = config.get('organization');
-		this.project = config.get('project');
-		this.team = config.get('team');
-		this.token = config.get('token');
-		this.debug = config.get<boolean>('debug', false);
-		this.defaultActivity = config.get('default.activity');
+                this.load(snippetsChanged).then(() => {
+                    logger.log('Configuration reloaded');
+                    this._onDidChange.fire(this);
+                });
+            }
+        });
+    }
 
-		const snippets = config.get<SnippetConfig>('snippets');
-		this.snippets = await this.loadSnippets(snippets);
-	}
+    private async load(loadSnippets: boolean) {
+        const config = vsc.workspace.getConfiguration(ConfigurationKey);
+        this.organization = config.get('organization');
+        this.project = config.get('project');
+        this.team = config.get('team');
+        this.token = config.get('token');
+        this.debug = config.get<boolean>('debug', false);
+        this.defaultActivity = config.get('default.activity');
 
-	private async loadSnippets(snippets?: SnippetConfig): Promise<SnippetConfig> {
-		if (!snippets || Object.keys(snippets).length === 0) {
-			return Promise.resolve({});
-		}
+        if (loadSnippets) {
+            const snippets = config.get<SnippetConfig>('snippets');
+            this.snippets = await this.loadSnippets(snippets);
+        }
+    }
 
-		const promises: Promise<any>[] = [];
-		const result: SnippetConfig = {};
+    private async loadSnippets(snippets?: SnippetConfig): Promise<SnippetConfig> {
+        if (!snippets || Object.keys(snippets).length === 0) {
+            return Promise.resolve({});
+        }
 
-		for (const key in snippets) {
-			promises.push(this.loadSingleSnippet(snippets[key]).then(data => {
-				result[key] = data;
-			}).catch((err: Error) => {
-				this.logger.log(`Error loading snippet '${key}': ${err.message}`, true);
-				console.log(err);
-				throw err;
-			}));
-		}
+        const promises: Promise<any>[] = [];
+        const result: SnippetConfig = {};
 
-		try {
-			await Promise.all(promises);
-		} catch(err) {
-			const seeDetailsAction = 'See details';
-			vsc.window.showErrorMessage('Some snippets could not have been loaded', seeDetailsAction).then(item => {
-				if (item === seeDetailsAction) {
-					this.logger.show();
-				}
-			});
-		}
+        for (const key in snippets) {
+            promises.push(this.loadSingleSnippet(snippets[key]).then(data => {
+                result[key] = data;
+            }).catch((err: Error) => {
+                this.logger.log(`Error loading snippet '${key}': ${err.message}`, true);
+                console.log(err);
+                throw err;
+            }));
+        }
 
-		return result;
-	}
+        try {
+            await Promise.all(promises);
+        } catch (err) {
+            const seeDetailsAction = 'See details';
+            vsc.window.showErrorMessage('Some snippets could not have been loaded', seeDetailsAction).then(item => {
+                if (item === seeDetailsAction) {
+                    this.logger.show();
+                }
+            });
+        }
 
-	private async loadSingleSnippet(url: string) {
-		if (url.startsWith('http')) {
-			if (this.debug) {
-				console.log(`[DEBUG] Getting ${url}`);
-			}
-			return Axios.get(url).then(r => r.data as string);
-		} else {
-			return new Promise<string>((resolve, reject) => {
-				if (this.debug) {
-					console.log(`[DEBUG] Reading ${url}`);
-				}
-				fs.readFile(url, {encoding: "UTF8"}, (err, data) => {
-					if (err) {
-						reject(err);
-					} else {
-						resolve(data);
-					}
-				});
-			});
-		}
-	}
+        return result;
+    }
 
-	dispose() {
-		this._eventHandler.dispose();
-		this._onDidChange.dispose();
-	}
+    private async loadSingleSnippet(url: string) {
+        if (url.startsWith('http')) {
+            if (this.debug) {
+                console.log(`[DEBUG] Getting ${url}`);
+            }
+            return Axios.get(url).then(r => r.data as string);
+        } else {
+            return new Promise<string>((resolve, reject) => {
+                if (this.debug) {
+                    console.log(`[DEBUG] Reading ${url}`);
+                }
+                fs.readFile(url, { encoding: "UTF8" }, (err, data) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(data);
+                    }
+                });
+            });
+        }
+    }
+
+    dispose() {
+        this._eventHandler.dispose();
+        this._onDidChange.dispose();
+    }
 }
 
 export interface SnippetConfig {
-	[name: string]: string;
+    [name: string]: string;
 }
