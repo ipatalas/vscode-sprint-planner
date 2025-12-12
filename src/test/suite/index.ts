e5312d1a0196ff6c149aecd251b63d8fbf6bd3b0
Ireneusz Patalas <fido@list.pl>
2025-12-12 22:31:42 +0100

    Bump packages

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -12,27 +12,26 @@ export function run(): Promise<void> {
     const testsRoot = path.resolve(__dirname, '..');
 
     return new Promise((c, e) => {
-        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-            if (err) {
+        glob('**/**.test.js', { cwd: testsRoot })
+            .then(files => {
+                // Add files to the test suite
+                files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+                try {
+                    // Run the mocha test
+                    mocha.run(failures => {
+                        if (failures > 0) {
+                            e(new Error(`${failures} tests failed.`));
+                        } else {
+                            c();
+                        }
+                    });
+                } catch (err) {
+                    e(err);
+                }
+            })
+            .catch(err => {
                 return e(err);
-            }
-
-            // Add files to the test suite
-            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
-            try {
-                // Run the mocha test
-                mocha.run(failures => {
-                    if (failures > 0) {
-                        e(new Error(`${failures} tests failed.`));
-                    } else {
-                        c();
-                    }
-                });
-            } catch (err) {
-                console.error(err);
-                e(err);
-            }
-        });
+            });
     });
 }
